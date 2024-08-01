87a7f6418f5e9d8f1a27a2937647bb1995624e7c
nchoals <nicholaspoh05@gmail.com>
2024-08-01 14:56:57 +0800

    update requirements

--- app.py
+++ app.py
@@ -1,67 +1,10 @@
-from flask import Flask, request, jsonify
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import google.generativeai as genai
-from langchain.vectorstores import FAISS
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.question_answering import load_qa_chain
-from langchain.prompts import PromptTemplate
-import os
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
-
-# Configure the API key
-API_KEY = os.getenv("API_KEY")
-genai.configure(api_key=API_KEY)
-
-# Initialize the chatbot model
-model = genai.GenerativeModel(
-    model_name='gemini-pro',
-    safety_settings={
-        genai.types.HarmCategory.HARM_CATEGORY_HATE_SPEECH: genai.types.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-        genai.types.HarmCategory.HARM_CATEGORY_HARASSMENT: genai.types.HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    }
-)
-chat = model.start_chat(history=[])
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/chat', methods=['POST'])
-def chat():
-    data = request.json
-    user_message = data.get('message')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    # Placeholder function to get response from chatbot logic
-    response = get_response(user_message)
-    return jsonify({"response": response})
-
-def get_response(user_message):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")  # type: ignore
-    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
-    docs = new_db.similarity_search(user_message)
-
-    chain = get_conversational_chain()
-
-    context = "\n".join([doc.page_content for doc in docs])
-    response = chain({"input_documents": docs, "context": context, "question": user_message}, return_only_outputs=True)
-
-    return response['output_text']
-
-def get_conversational_chain():
-    prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details.\n\n
-    Context:\n{context}?\n
-    Question:\n{question}\n
-    Answer:
-    """
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
-                                   client=genai,
-                                   temperature=0.3,
-                                   )
-    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
-    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
-    return chain
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
